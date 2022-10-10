886ebba31f659c5af25c825282ba10854f3e08fd
DefinitelyObsessed <99426520+DefinitelyObsessed@users.noreply.github.com>
2022-10-10 11:00:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const form = document.querySelector("#form") ;
-const email = document.querySelector("#email") ;
-const submitButton = document.querySelector("#submit") ;
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const submitButton = document.querySelector("#submit");
 
-const emailRegEx =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i ;
+const emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 form.addEventListener('submit', (e) => {
-      var emailValue = email.value;
-      console.log(emailValue);
+  var emailValue = email.value;
+  console.log(emailValue);
 
-      if(emailRegEx.test(emailValue) === true) {
-            form.classList.remove('error');
-      } else{
-            form.classList.add('error');
-            e.preventDefault() ;
-      }
+  if (emailRegEx.test(emailValue) === true) {
+    form.classList.remove('error');
+  } else {
+    form.classList.add('error');
+    e.preventDefault();
+  }
 });
-
-
-
-
-
